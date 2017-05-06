ff69d1e9c584142bb0620dd7c2d14c1a4f567ff5
Hector Flores <hfloresr@uci.edu>
2017-05-06 11:04:04 -0700

    started qlearner

---
+++ sandhog/qlearner.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Qlearner for AI project
+"""
+
+import numpy as np
+
+
+def random_pair(s, e):
+    """TODO: Docstring for random_pair.
+    :returns: TODO
+
+    """
+    return np.random.randint(s, e), np.random.randint(s, e)
+
+
+def init_grid():
+    """TODO: Docstring for init_grid.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    state = None
+    return state
+
+
+def init_randplayer():
+    """TODO: Docstring for init_randplayer.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    state = None
+    return state
+
+
+def init_randgrid():
+    """TODO: Docstring for init_randgrid.
+    :returns: TODO
+
+    """
+    state = None
+    return state
