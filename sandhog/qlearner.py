552e849bf12df0cb4fce8ad5babf0c138252e3c2
Hector Flores <hfloresr@uci.edu>
2017-05-06 13:43:52 -0700

    added border fences

--- sandhog/qlearner.py
+++ sandhog/qlearner.py
@@ -37,17 +37,22 @@ def init_grid():
     :returns: state
 
     """
-    state = np.zeros((4, 4, 5))
+    state = np.zeros((9, 9, 5))
 
-    state[rand_pair(0, 4)] = np.array([1, 0, 0, 0, 0])  # AI agent
-    state[rand_pair(0, 4)] = np.array([0, 1, 0, 0, 0])  # Human agent
-    state[rand_pair(0, 4)] = np.array([0, 0, 1, 0, 0])  # Pig
+    state[rand_pair(2, 7)] = np.array([1, 0, 0, 0, 0])  # AI agent
+    state[rand_pair(2, 7)] = np.array([0, 1, 0, 0, 0])  # Human agent
+    state[rand_pair(2, 7)] = np.array([0, 0, 1, 0, 0])  # Pig
 
     # Initialize fences
+    for i in [1, 7]:
+        for j in range(1, 8):
+            state[i, j] = np.array([0, 0, 0, 1, 0])
+            if j != 4:
+                state[j, i] = np.array([0, 0, 0, 1, 0])
     state[3, 3] = np.array([0, 0, 0, 1, 0])
     state[5, 3] = np.array([0, 0, 0, 1, 0])
-    state[5, 3] = np.array([0, 0, 0, 1, 0])
-    state[5, 3] = np.array([0, 0, 0, 1, 0])
+    state[3, 5] = np.array([0, 0, 0, 1, 0])
+    state[5, 5] = np.array([0, 0, 0, 1, 0])
 
     # Initialize exits
     state[4, 1] = np.array([0, 0, 0, 0, 1])
