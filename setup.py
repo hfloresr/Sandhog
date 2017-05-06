717e09e659524ae9ec56ad14da544b159dc05096
Hector Flores <hfloresr@uci.edu>
2017-05-06 10:32:09 -0700

    restructed fileystem for project

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='Sandhog',
+    version='0.1.0',
+    description='Minecraft AI Collaborative Challenge',
+    author='Hector Flores, Amanda Ortscheid, Malav Pandya',
+    author_email='hfloresr@uci.edu, aortscheid@uci.edu, mcpandya@uci.edu',
+    url='https://hfloresr.github.io/Sandhog',
+    packages=['sandhog']
+)
