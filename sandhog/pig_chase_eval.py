9d5abb4650b9cee3122a246eb1affa0ee3fbfd3a
Hector Flores <hfloresr@uci.edu>
2017-05-21 20:44:39 -0700

    added sketch for evaluating agent

---
+++ sandhog/pig_chase_eval.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from common import ENV_AGENT_NAMES
+from evaluation import PigChaseEvaluator
+from environment import PigChaseTopDownStateBuilder
+from malmopy.agent import RandomAgent
+
+
+if __name__ == '__main__':
+    # TODO: Evaluate agent
+
+    #agent_100k = MyCustomAgent()
+    #agent_500k = MyCustomAgent()
+
+    #eval = PigChaseEvaluator(agent_100k, agent_500k, PigChaseTopDownStateBuilder())
+    #eval.run()
+    #eval.save('sandhog_exp01', 'pig_chase_results.json')
